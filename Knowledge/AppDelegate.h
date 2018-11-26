cc442d0751012571e22125f7a450e8d01ac3f311
Lee <lirihuang@ppdai.com>
2018-11-26 11:30:36 +0800

    Initial Commit

---
+++ Knowledge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Knowledge
+//
+//  Created by Lee on 2018/11/26.
+//  Copyright © 2018年 PPD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
